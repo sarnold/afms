fc41f51f81abf1e1bb343bf0ceebe94aaa27e4d9
achimk <achimk@2c6dc2f6-b447-0410-a04b-ddf0567562f5>
2008-06-21 20:02:45 +0000

    * _afdocutils.py (html_parts): changed overrides to support py2exe
    * _afhelper.py (we_are_frozen, module_path): new functions
    
    * template.txt, html4css1.css, setup.py: new files for Win32 binaries using py2exe

--- trunk/_afhelper.py
+++ trunk/_afhelper.py
@@ -21,6 +21,7 @@
 
 # $Id$
 
+import os, sys
 import wx
 
 
@@ -75,3 +76,20 @@ def DontAnnoyMessageBox(title, message):
     dont_annoy = dlg.cb.GetValue()
     dlg.Destroy()
     return (dlgresult, dont_annoy)
+
+
+def we_are_frozen():
+    """Returns whether we are frozen via py2exe.
+    This will affect how we find out where we are located."""
+
+    return hasattr(sys, "frozen")
+
+
+def module_path():
+    """ This will get us the program's directory,
+    even if we are frozen using py2exe"""
+
+    if we_are_frozen():
+        return os.path.dirname(unicode(sys.executable, sys.getfilesystemencoding( )))
+
+    return os.path.dirname(unicode(__file__, sys.getfilesystemencoding( )))
