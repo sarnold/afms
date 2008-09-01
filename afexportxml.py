c8273ecbee79275a9580801d78482646b30b9fac
achimk <achimk@2c6dc2f6-b447-0410-a04b-ddf0567562f5>
2008-09-01 19:39:20 +0000

    * test_afexportxml.py: renamed to afexportxml.py
    * CHANGELOG.txt: updated

--- afexportxml.py
+++ afexportxml.py
@@ -36,157 +36,195 @@ if __name__=="__main__":
     LOCALEDIR = os.path.join(basepath, 'locale')
     DOMAIN = "afms"
     gettext.install(DOMAIN, LOCALEDIR, unicode=True)
-
-import codecs
+import codecs, logging
+from xml.dom.minidom import getDOMImplementation, XMLNS_NAMESPACE, parseString, parse
 from time import localtime, gmtime, strftime
-import afmodel
-import afconfig, afresource
+import afmodel, afconfig, afresource, _afartefact, _afhtmlwindow
 from afresource import ENCODING
+from afexporthtml import afExportXMLBase
 
 
-class afExportXML():
-    def __init__(self, outfilename, model):
+
+class afExportXML(afExportXMLBase):
+    def __init__(self, model):
         self.model = model
-        self.of = codecs.open(outfilename, encoding=ENCODING, mode="w", errors='strict')
-        self.writeXMLHeader()
-        self.writeProductInfo()
-        self.writeSimpleSections()
-        self.writeGlossary()
-        self.writeFeatures()
-        self.writeRequirements()
-        self.writeTestcases()
-        self.writeTestsuites()
-        self.writeRelations()
-        self.writeXMLFooter()
-        self.of.close()
-
-
-    def writeXMLHeader(self):
-        self.of.write('<?xml version="1.0" encoding="UTF-8"?>\n')
-        self.of.write('<!-- Created from %s at %s !-->\n' % (self.model.getFilename(), strftime(afresource.TIME_FORMAT, localtime())))
-        self.of.write('<product>\n')
-
-
-    def writeXMLFooter(self):
-        self.of.write('</product>\n')
-
-
-    def writeTag(self, tag, content):
-        self.of.write("<%s><![CDATA[%s]]></%s>\n" % (tag, content, tag))
-
-
-    def writeSimpleTag(self, tag, content):
-        self.of.write("<%s>%s</%s>\n" % (tag, content, tag))
-
-
-    def writeProductInfo(self):
-        pi = self.model.getProductInformation()
-        self.writeTag('producttitle', pi['title'])
-        self.writeTag('productdescription', pi['description'])
-
-
-    def writeSimpleSections(self):
+        self.title='AFMS Report'
+        self.encoding = 'UTF-8'
+        
+        self.impl = getDOMImplementation()
+        self.xmldoc = self.impl.createDocument(None, "artefacts", None)
+        self.root = self.xmldoc.documentElement
+        self.root.setAttribute('source', self.model.getFilename())
+        self.root.setAttribute('creationdate', strftime(afresource.TIME_FORMAT, localtime()))
+        
+    
+    def run(self):
+        # --- Product information ---
+        node = self.renderProductInformation()
+        self.root.appendChild(node)
+        
+        # --- Text sections ---
         idlist = self.model.getSimpleSectionIDs()
-        self.of.write('<simplesections>\n')
-        for ID in idlist:
-            simplesection = self.model.getSimpleSection(ID)
-            self.of.write(simplesection.xmlrepr('simplesection'))
-        self.of.write('</simplesections>\n')
+        for id in idlist:
+            node = self.renderSimpleSection(id)
+            self.root.appendChild(node)
+            
+        # --- Glossary ---
+        cursor = self.model.connection.cursor()
+        # SQL query for demonstration purposes only
+        cursor.execute('select ID from glossary where delcnt==0 order by title;')
+        for id in [item[0] for item in cursor.fetchall()]:
+            node = self.renderGlossaryEntry(id)
+            self.root.appendChild(node)
 
-
-    def writeGlossary(self):
-            idlist = self.model.getGlossaryEntryIDs()
-            self.of.write('<glossary>\n')
-            for ID in idlist:
-                glossaryentry = self.model.getGlossaryEntry(ID)
-                self.of.write(glossaryentry.xmlrepr('glossaryentry'))
-            self.of.write('</glossary>\n')
-
-
-    def writeFeatures(self):
+        # --- Features ---
         idlist = self.model.getFeatureIDs()
-        self.of.write('<features>\n')
-        for ID in idlist:
-            feature = self.model.getFeature(ID)
-            self.of.write(feature.xmlrepr())
-        self.of.write('</features>\n')
+        for id in idlist:
+           node = self.renderFeature(id)
+           self.root.appendChild(node)
 
-
-    def writeRequirements(self):
+        # --- Requirements ---
         idlist = self.model.getRequirementIDs()
-        self.of.write('<requirements>\n')
-        for ID in idlist:
-            requirement = self.model.getRequirement(ID)
-            self.of.write(requirement.xmlrepr())
-        self.of.write('</requirements>\n')
+        for id in idlist:
+           node = self.renderRequirement(id)
+           self.root.appendChild(node)
 
-
-    def writeUsecase(self, uc_id):
-        idlist = self.model.getUsecaseIDs()
-        self.of.write('<usecases>\n')
-        for ID in idlist:
-            usecase = self.model.getUsecase(ID)
-            self.of.write(usecase.xmlrepr())
-        self.of.write('</usecases>\n')
-
-
-    def writeTestcases(self):
+        # --- Testcases ---
         idlist = self.model.getTestcaseIDs()
-        self.of.write('<testcases>\n')
-        for ID in idlist:
-            testcase = self.model.getTestcase(ID)
-            self.of.write(testcase.xmlrepr())
-        self.of.write('</testcases>\n')
+        for id in idlist:
+           node = self.renderTestcase(id)
+           self.root.appendChild(node)
 
+        # --- Usecases ---
+        idlist = self.model.getUsecaseIDs()
+        for id in idlist:
+           node = self.renderUsecase(id)
+           self.root.appendChild(node)
 
-    def writeTestsuites(self):
+        # --- Usecases ---
         idlist = self.model.getTestsuiteIDs()
-        self.of.write('<testsuites>\n')
-        for ID in idlist:
-            testsuite = self.model.getTestsuite(ID)
-            self.of.write(testsuite.xmlrepr())
-        self.of.write('</testsuites>\n')
+        for id in idlist:
+           node = self.renderTestsuite(id)
+           self.root.appendChild(node)
 
 
-    def writeRelations(self):
-        data = [\
-            {   'func'          : self.model.getFeatureRequirementRelations,
-                'outertag'      : 'feature_requirement_relations',
-                'innerlefttag'  : 'feature_id',
-                'innerrighttag' : 'requirement_id'
-            },
-            {   'func'          : self.model.getTestsuiteTestcaseRelations,
-                'outertag'      : 'testsuite_testcase_relations',
-                'innerlefttag'  : 'testsuite_id',
-                'innerrighttag' : 'testcase_id'
-            },
-            {   'func'         : self.model.getRequirementUsecaseRelations,
-                'outertag'      : 'requirement_usecase_relations',
-                'innerlefttag'  : 'requirement_id',
-                'innerrighttag' : 'usecase_id'
-            },
-            {   'func'          : self.model.getRequirementTestcaseRelations,
-                'outertag'      : 'requirement_testcase_relations',
-                'innerlefttag'  : 'requirement_id',
-                'innerrighttag' : 'testcase_id'
-            },
-            {   'func'          : self.model.getRequirementRequirementRelation,
-                'outertag'      : 'requirement_requirement_relations',
-                'innerlefttag'  : 'requirement1_id',
-                'innerrighttag' : 'requirement2_id'
-            }
-            ]
+    def renderProductInformation(self):
+        productinfo = self.model.getProductInformation()
+        node = self._createElement('productinformation')
+        node.appendChild(self._createTextElement('title', productinfo['title']))
+        node.appendChild(self._render(productinfo['description'], enclosingtag='description'))
+        return node
 
-        for d in data:
-            self.of.write('<%s>\n' % d["outertag"])
-            relations = d["func"]()
-            for r in relations:
-                self.of.write('<relation>\n')
-                self.writeSimpleTag(d['innerlefttag'], r[0])
-                self.writeSimpleTag(d['innerrighttag'], r[1])
-                self.of.write('</relation>\n')
-            self.of.write('</%s>\n' % d["outertag"])
+    
+    def renderSimpleSection(self, ID):
+        simplesection = self.model.getSimpleSection(ID)
+        node = self._createElement('simplesection', {'ID' : str(ID)})
+        node.appendChild(self._createTextElement('title', simplesection['title']))
+        node.appendChild(self._render(simplesection['content'], enclosingtag='content'))
+        changelognode = self.renderChangelist(simplesection)
+        node.appendChild(changelognode)
+        return node
+    
+    
+    def renderGlossaryEntry(self, ID):
+        glossaryentry = self.model.getGlossaryEntry(ID)
+        node = self._createElement('glossaryentry', {'ID' : str(ID)})
+        node.appendChild(self._createTextElement('title', glossaryentry['title']))
+        node.appendChild(self._render(glossaryentry['description'], enclosingtag='description'))
+        return node
+        
 
+    def renderFeature(self, ID):
+        feature = self.model.getFeature(ID)
+        basedata = feature.getPrintableDataDict()
+        node = self._createElement('feature', {'ID' : str(ID)})
+        for key in ('priority', 'status', 'version', 'risk'):
+            node.appendChild(self._createTextElement(key, basedata[key]))        
+        node.appendChild(self._render(basedata['description'], enclosingtag='description'))
+        node.appendChild(self.renderRelatedArtefacts('relatedrequirements', feature.getRelatedRequirements()))
+        node.appendChild(self.renderChangelist(feature))
+        return node
+
+
+    def renderRequirement(self, ID):
+        requirement = self.model.getRequirement(ID)
+        basedata = requirement.getPrintableDataDict()
+        node = self._createElement('requirement', {'ID': str(ID)})
+        for key in ('priority', 'status', 'version', 'complexity', 'assigned', 'effort', 'category'):
+            node.appendChild(self._createTextElement(key,    basedata[key]))
+        for key in ('description', 'origin', 'rationale'):
+            node.appendChild(self._render(basedata[key], enclosingtag=key))
+        node.appendChild(self.renderRelatedArtefacts('relatedfeatures', requirement.getRelatedFeatures()))
+        node.appendChild(self.renderRelatedArtefacts('relatedrequirements', requirement.getRelatedRequirements()))
+        node.appendChild(self.renderRelatedArtefacts('relatedusecases', requirement.getRelatedUsecases()))
+        node.appendChild(self.renderRelatedArtefacts('relatedtestcases', requirement.getRelatedTestcases()))
+        node.appendChild(self.renderChangelist(requirement))
+        return node
+    
+    
+    def renderTestcase(self, ID):
+        testcase = self.model.getTestcase(ID)
+        basedata = testcase.getPrintableDataDict()
+        node = self._createElement('testcase', {'ID': str(ID)})
+        node.appendChild(self._createTextElement('title',    basedata['title']))
+        node.appendChild(self._createTextElement('version',    basedata['version']))
+        for key in ('purpose', 'prerequisite', 'testdata', 'steps', 'notes'):
+            node.appendChild(self._render(basedata[key], enclosingtag=key))
+        node.appendChild(self.renderRelatedArtefacts('relatedrequirements', testcase.getRelatedRequirements()))
+        node.appendChild(self.renderRelatedArtefacts('relatedtestsuites', testcase.getRelatedTestsuites()))
+        node.appendChild(self.renderChangelist(testcase))
+        return node
+
+
+    def renderUsecase(self, ID):
+        usecase = self.model.getUsecase(ID)
+        basedata = usecase.getPrintableDataDict()
+        node = self._createElement('usecase', {'ID': str(ID)})
+        for key in ('title', 'priority', 'usefrequency', 'actors', 'stakeholders'): 
+            node.appendChild(self._createTextElement(key, basedata[key]))
+        for key in ('prerequisites', 'mainscenario', 'altscenario', 'notes'):
+            node.appendChild(self._render(basedata[key], enclosingtag=key))
+        node.appendChild(self.renderRelatedArtefacts('relatedrequirements', usecase.getRelatedRequirements()))
+        node.appendChild(self.renderChangelist(usecase))
+        return node
+    
+    
+    def renderTestsuite(self, ID):
+        testsuite = self.model.getTestsuite(ID)
+        basedata = testsuite.getPrintableDataDict()
+        node = self._createElement('testsuite', {'ID': str(ID)})
+        for key in ('title', 'execorder', 'nbroftestcase'):
+            node.appendChild(self._createTextElement(key, basedata[key]))
+        node.appendChild(self._render(basedata['description'], enclosingtag='description'))  
+        node.appendChild(self.renderRelatedArtefacts('relatedtestcases', testsuite.getRelatedTestcases()))
+        return node
+
+
+    def renderRelatedArtefacts(self, nodename, artefactlist):
+        node = self._createElement(nodename)
+        for artefact in artefactlist:
+            node.appendChild(self._createTextElement('ID', str(artefact['ID'])))
+        return node
+
+
+    def renderChangelist(self, artefact):
+        node = self._createElement('changelist')
+        changelist = artefact.getChangelist()
+        for changelogentry in changelist:
+            basedata = changelogentry.getPrintableDataDict()
+            subnode = self._createElement('changelogentry', {'ID': str(basedata['ID'])})
+            subnode.appendChild(self._createTextElement('user', str(basedata['user'])))
+            subnode.appendChild(self._render(basedata['description'], enclosingtag='description'))
+            subnode.appendChild(self._createTextElement('changetype', str(basedata['changetype'])))
+            subnode.appendChild(self._createTextElement('date', basedata['date']))
+            node.appendChild(subnode)
+        return node
+
+
+def doExportHTML(path, model):
+    export = afExportXML(model)
+    export.run()
+    export.write(path)
 
 
 if __name__=="__main__":
@@ -228,6 +266,9 @@ if __name__=="__main__":
         usage()
         sys.exit(1)
 
+    logging.basicConfig(level=afconfig.loglevel, format=afconfig.logformat)
+    logging.disable(afconfig.loglevel)
+
     model = afmodel.afModel(controller = None)
     try:
         cwd = os.getcwd()
@@ -240,4 +281,4 @@ if __name__=="__main__":
     if output is None:
         output =  os.path.splitext(args[0])[0] + ".html"
 
-    export = afExportXML(output, model)
+    doExportHTML(output, model)
