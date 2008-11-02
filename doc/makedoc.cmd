16735ddbdf5cd7704b39f51aec8523c5516761b7
achimk <achimk@2c6dc2f6-b447-0410-a04b-ddf0567562f5>
2008-11-02 20:25:19 +0000

    * added some nice gimmicks in documentation

--- doc/makedoc.cmd
+++ doc/makedoc.cmd
@@ -3,10 +3,13 @@ rem Create HTML documentation using docutils
 
 
 python patch_release_info.py index.txt > tmp.txt
-call rst2html --stylesheet=afmsdoc.css tmp.txt index.html
-del tmp.txt
-call rst2html --stylesheet=afmsdoc.css afeditor.txt afeditor.html
-call rst2html --stylesheet=afmsdoc.css testrunner.txt testrunner.html
-call rst2html --stylesheet=afmsdoc.css markup.txt markup.html
-call rst2html --stylesheet=afmsdoc.css acknowledgement.txt acknowledgement.html
-call rst2html --stylesheet=afmsdoc.css changelog.txt changelog.html
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css tmp.txt index.html
+del tmp.txt                      
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css afeditor.txt afeditor.htm
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css testrunner.txt testrunner.html
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css markup.txt markup.html
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css acknowledgement.txt acknowledgement.html
+call rst2html --link-stylesheet --stylesheet=afmsdoc.css changelog.txt changelog.html
+                              
+
+python patch_html_head.py afeditor.htm > afeditor.html
